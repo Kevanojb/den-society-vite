a7891bafa823e3e3f7a6d89a6e9d2485fedead4e
Kevanojb <kevanojb@icloud.com>
2026-01-22 16:39:06 +0000

    Den League Vite app

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        squab: {
+          50: "#eefbf2",
+          100: "#d8f5e3",
+          200: "#b4eac9",
+          300: "#86dbab",
+          400: "#55c787",
+          500: "#2fb66a",
+          600: "#199f57",
+          700: "#147f47",
+          800: "#12663b",
+          900: "#0f5232",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
