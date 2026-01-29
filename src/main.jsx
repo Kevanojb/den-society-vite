cd5e1ff0cc7933b4d32f0c803ce6d2e63a15f8ef
Kevanojb <132914277+Kevanojb@users.noreply.github.com>
2026-01-29 14:40:43 +0000

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./legacy.css";         // ✅ THIS is what makes the “special cards” work
-import AuthGate from "./AuthGate.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthGate />
+    {/* BASE_URL will be "/golf/" on GitHub Pages because of Vite base */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
