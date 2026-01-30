e66004b37deaeed52e08baa206041bd18d3acb31
Kevanojb <132914277+Kevanojb@users.noreply.github.com>
2026-01-30 17:04:55 +0000

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import AuthGate from "./AuthGate.jsx"; // <-- add this
+import Bootstrap from "./Bootstrap.jsx";
 import "./index.css";
 import "./legacy.css";
+import "./legacy-2.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <AuthGate />
+      <Bootstrap />
     </BrowserRouter>
   </React.StrictMode>
 );
